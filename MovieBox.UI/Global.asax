3158fa4fef97c7a0309c98aa1b36f28dd43ed74e
Cem Yaraş <cemyaras@gmail.com>
2020-10-16 07:18:46 +0300

    Adding global.asax and RouteConfig classes, adding and updating packages

---
+++ MovieBox.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieBox.Ui.Global" Language="C#" %>
---
+++ MovieBox.UI/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using MovieBox.Business.Services;
+using MovieBox.Data.Core;
+using MovieBox.Ui.App_Start;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MovieBox.Ui
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<RottenTomatoService>().As<IRottenTomatoService>();
+            builder.RegisterType<FetchService>().As<IFetchService>();
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            AreaRegistration.RegisterAllAreas();
+            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+    }
+}
