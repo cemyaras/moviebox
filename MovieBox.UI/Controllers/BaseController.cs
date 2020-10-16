df50e30f2b72573954fed74aecd111b8f198b31f
Cem Yaraş <cemyaras@gmail.com>
2020-10-16 07:20:39 +0300

    Adding base and movie controllers

---
+++ MovieBox.UI/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace MovieBox.Ui.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected int pageSize = 10;
+    }
+}
---
+++ MovieBox.UI/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using MovieBox.Business.Services;
+using System.Web.Mvc;
+
+namespace MovieBox.Ui.Controllers
+{
+    public class MovieController : BaseController
+    {
+        private readonly IRottenTomatoService _service;
+        public MovieController(IRottenTomatoService service)
+        {
+            _service = service;
+        }
+
+        public ActionResult Index()
+        {
+            var boxscoreList = _service.GetBoxOfficeMovieList();
+            return View(boxscoreList);
+        }
+
+        public ActionResult Search(string keyword, int page = 1)
+        {
+            var model = _service.SearchMovies(keyword, pageSize, page);
+            ViewBag.Page = page;
+            ViewBag.MaxPageLimit = model.Total / pageSize;
+            ViewBag.Keyword = keyword;
+
+            return View(model);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var model = _service.GetMovieDetails(id);
+            return View(model);
+        }
+
+        //public ActionResult BoxOffice()
+        //{
+        //    var boxscoreList = _service.GetBoxOfficeMovieList();
+        //    return View(boxscoreList);
+        //}
+    }
+}
